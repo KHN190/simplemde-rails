3eb87d75fd5f29de7cec2dbaaad746855220cd94
knh190 <knh190@hotmail.com>
2017-10-25 13:48:52 +0800

    update to simplemde 1.11.2.

--- lib/simplemde.rb
+++ lib/simplemde.rb
@@ -2,9 +2,9 @@ require "simplemde/version"
 
 # module Simplemde
 module Rails
-	class Simplemde < Engine
-		initializer :assets do |config|
-			Rails.application.config.assets.precompile += %w( simplemde/simplemde.min.js simplemde/simplemde.min.css )
-		end
-	end
+  class Simplemde < Engine
+    initializer :assets do |config|
+      Rails.application.config.assets.precompile += %w( simplemde/simplemde.min.js simplemde/simplemde.min.css )
+    end
+  end
 end
--- lib/simplemde/version.rb
+++ lib/simplemde/version.rb
@@ -1,5 +1,5 @@
 module Simplemde
   module Rails
-    VERSION = "1.10.2"
+    VERSION = "1.11.2"
   end
 end
